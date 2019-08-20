6bddb6d2108d79f6adee882371ee0f9210238d58
Monty Montgomery <xiphmont@gmail.com>
2019-08-20 15:47:16 -0400

    cdef_dist-only test

--- src/rdo.rs
+++ src/rdo.rs
@@ -1261,8 +1261,12 @@ fn rdo_loop_plane_error<T: Pixel>(sbo: SuperBlockOffset, tile_sbo: SuperBlockOff
           let test_bo = sbo.block_offset(bx << 1, by << 1);
           let test_region = test_plane.region(Area::BlockStartingAt { bo: test_bo });
 
-            err += sse_wxh(&in_region, &test_region, 8 >> xdec, 8 >> ydec);
-         }
+          //if pli==0 {
+            err += cdef_dist_wxh_8x8(&in_region, &test_region, fi.sequence.bit_depth);
+          //} else {
+            //err += sse_wxh(&in_region, &test_region, 8 >> xdec, 8 >> ydec);
+          //}
+        }
       }
     }
   }
